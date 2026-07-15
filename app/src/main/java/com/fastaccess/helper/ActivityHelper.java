d3d93b01e3236ea5e73503a6cc54aaf3fbd3f59d
lll69 <60803753+lll69@users.noreply.github.com>
2026-07-15 09:39:59 +0800

    fix some browsers not showing up in the selector

--- app/src/main/java/com/fastaccess/helper/ActivityHelper.java
+++ app/src/main/java/com/fastaccess/helper/ActivityHelper.java
@@ -74,8 +74,10 @@ public class ActivityHelper {
     }
 
     private static void openChooser(@NonNull Context context, @NonNull Uri url, boolean fromCustomTab) {
-        Intent i = new Intent(Intent.ACTION_VIEW, url);
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setDataAndType(url, "text/html");
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addCategory(Intent.CATEGORY_BROWSABLE);
         Intent finalIntent = chooserIntent(context, i, url);
         if (finalIntent != null) {
             try {
@@ -219,33 +221,7 @@ public class ActivityHelper {
     }
 
     private static Intent chooserIntent(@NonNull Context context, @NonNull Intent intent, @NonNull Uri uri) {
-        final PackageManager pm = context.getPackageManager();
-        final List<ResolveInfo> activities = pm.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-        final ArrayList<Intent> chooserIntents = new ArrayList<>();
-        final String ourPackageName = context.getPackageName();
-        for (ResolveInfo resInfo : activities) {
-            ActivityInfo info = resInfo.activityInfo;
-            if (!info.enabled || !info.exported) {
-                continue;
-            }
-            if (info.packageName.equals(ourPackageName)) {
-                continue;
-            }
-            Intent targetIntent = new Intent(intent);
-            targetIntent.setPackage(info.packageName);
-            targetIntent.setDataAndType(uri, intent.getType());
-            chooserIntents.add(targetIntent);
-        }
-        if (chooserIntents.isEmpty()) {
-            return null;
-        }
-        final Intent lastIntent = chooserIntents.remove(chooserIntents.size() - 1);
-        if (chooserIntents.isEmpty()) {
-            return lastIntent;
-        }
-        Intent chooserIntent = Intent.createChooser(lastIntent, null);
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, chooserIntents.toArray(new Intent[chooserIntents.size()]));
-        return chooserIntent;
+        return Intent.createChooser(intent, null);
     }
 
     public static void activateLinkInterceptorActivity(Context context, boolean activate) {
